dd50921d8cc950eb5d0caa2746598f9227a53bdf
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2024-01-05 15:34:24 +0500

    Shop page ShopProducts component is done

---
+++ src/components/Shop/ShopProducts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="blogs" class="flex flex-wrap justify-center mb-10 gap-10">
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+  </div>
+  <div id="pagination" class="flex justify-center">
+    <el-pagination background layout="prev, pager, next" :total="110" />
+  </div>
+</template>
+
+<script setup>
+import Product from "../Main/Product.vue";
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/ShopPage.vue
+++ src/pages/ShopPage.vue
@@ -2,12 +2,14 @@
   <Header />
   <ShopImage />
   <ShopFilter />
+  <ShopProducts />
 </template>
 
 <script setup>
 import Header from "../components/Main/Header.vue";
 import ShopImage from "../components/Shop/ShopImage.vue";
 import ShopFilter from "../components/Shop/ShopFilter.vue";
+import ShopProducts from "../components/Shop/ShopProducts.vue";
 </script>
 
 <style lang="scss" scoped></style>
