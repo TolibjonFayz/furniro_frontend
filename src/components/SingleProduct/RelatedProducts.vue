e164cc660dde221c3d3e694993079a59ff81e594
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2024-01-08 14:14:19 +0500

    Single page is done

---
+++ src/components/SingleProduct/RelatedProducts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main" class="flex flex-col items-center gap-11 pt-5 mb-24">
+    <h1 class="font-['Poppins'] text-[36px] font-medium">Related Products</h1>
+    <div class="flex gap-5 flex-wrap justify-center">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+    <el-button type="warning" plain style="width: 120px">Show more</el-button>
+  </div>
+</template>
+
+<script setup>
+import Product from "../Main/Product.vue";
+</script>
+
+<style lang="scss" scoped>
+#main {
+  border-top: 1px solid #d9d9d9;
+}
+</style>
--- src/pages/SingleProduct.vue
+++ src/pages/SingleProduct.vue
@@ -3,6 +3,8 @@
   <SPWay />
   <SingleProduct />
   <Description />
+  <RelatedProducts />
+  <Footer />
 </template>
 
 <script setup>
@@ -10,6 +12,8 @@ import Header from "../components/Main/Header.vue";
 import SPWay from "../components/SingleProduct/SPWay.vue";
 import SingleProduct from "../components/SingleProduct/SingleProduct.vue";
 import Description from "../components/SingleProduct/Description.vue";
+import RelatedProducts from "../components/SingleProduct/RelatedProducts.vue";
+import Footer from "../components/Main/Footer.vue";
 </script>
 
 <style lang="scss" scoped></style>
